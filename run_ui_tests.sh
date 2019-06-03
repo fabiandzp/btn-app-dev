0981f6e8f2e418e56ee47819ad1cc14704b452c5
Fabian Zabala <fabiandzp@live.com>
2019-06-02 21:12:17 -0500

    test 102

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -9,6 +9,6 @@ curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
-   -H "Authorization: gQJRA1sLADxvYs2O02fllg" \
+   -H "Authorization: token gQJRA1sLADxvYs2O02fllg" \
    -d "$body" \
    https://api.travis-ci.com/repo/fabiandzp%2Fdemo_ut/requests
