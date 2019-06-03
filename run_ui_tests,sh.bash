8770ff3435379426aa26834020bb82a54b506866
Fabian Zabala <fabiandzp@live.com>
2019-06-02 19:05:13 -0500

    trigger test

---
+++ run_ui_tests,sh.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: jlWBNhWkz9P2Y-iwhd1Djw" \
+   -d "$body" \
+   https://api.travis-ci.com/repo/fabiandzp%2Fdemo_ut/requests
